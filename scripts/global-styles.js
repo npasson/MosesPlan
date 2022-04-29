b6292e218e59c251a0d26d2e9ad98cedcf30dc4c
npasson <37216467+npasson@users.noreply.github.com>
2022-04-29 21:36:56 +0200

    Fix critical Chrome runtime bug

--- scripts/global-styles.js
+++ scripts/global-styles.js
@@ -1,4 +1,10 @@
 function getGlobalStyles() {
-	let css_url = browser.runtime.getURL("style/global.css");
+	let css_url;
+	if ( typeof browser !== 'undefined' ) {
+		css_url = browser.runtime.getURL( 'style/global.css' );
+	} else {
+		css_url = chrome.runtime.getURL( 'style/global.css' );
+	}
+
 	return fetch( css_url );
 }
